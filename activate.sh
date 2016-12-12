2a52ac6c3392c647a9869ad00cb73c71fe5d5d89
Victor Escorcia <escorciav@gmail.com>
2016-12-12 16:19:46 +0300

    [repo] Update repo management/config files
    Hide personalized scripts for activate/deactivate the environment.
    Activate conda environment even during race condition.

--- activate.sh
+++ activate.sh
@@ -1,3 +1,11 @@
 module purge
-module load apps/conda
-source activate video-utils
+module load conda
+
+while true; do
+  source activate video-utils
+  if [ $? -eq 0 ]; then
+    break;
+  else
+     sleep $[ ( $RANDOM % 10 ) + 1 ]s
+  fi
+done
