bfa43e7280472c3d78a0b2db91bca20cc90a3ebc
Victor Escorcia <escorciav@gmail.com>
2016-12-12 10:48:31 +0300

    [repo] Initial commit
    Create repo with video-utilities

---
+++ activate.sh
@@ -0,0 +1,3 @@
+module purge
+module load apps/conda
+source activate video-utils
---
+++ setup.sh
@@ -0,0 +1,4 @@
+conda env create -f environment_x64.yml
+# Originally created by:
+#conda create -n video-utils python=3.5
+#conda install -c conda-forge ffmpeg
