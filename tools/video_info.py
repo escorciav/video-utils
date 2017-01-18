5f18cfaf7a67a29753e8ff7eff760d22f9d6180c
Victor Escorcia <escorciav@gmail.com>
2017-01-18 13:43:32 +0300

    [tools] Include number of frames in video_info
    Add number of frames of vides in video_info.py tool. Minor changes
    related with dependencies and computation were added. The main changes
    are described here:
    
    - Remove numpy dependency
    
    - Add video-name during stat generation instead of concatenating two
      DataFrame Tables.

--- tools/video_info.py
+++ tools/video_info.py
@@ -5,18 +5,20 @@ Python program to dump CSV with duration and frame rate of many videos
 
 """
 import argparse
+import os
 
-import numpy as np
 import pandas as pd
 from joblib import Parallel, delayed
 
-from okvideo.ffmpeg import get_duration, get_frame_rate
+from okvideo.ffmpeg import get_duration, get_frame_rate, get_num_frames
 
 
 def video_stats(filename):
     stats = []
+    stats.append(os.path.basename(os.path.splitext(filename)[0]))
     stats.append(get_duration(filename))
     stats.append(get_frame_rate(filename))
+    stats.append(get_num_frames(filename))
     return stats
 
 
@@ -24,12 +26,13 @@ def input_parse():
     description = 'Get information (duration, frame-rate) of several videos.'
     p = argparse.ArgumentParser(description=description)
     p.add_argument('-if', '--input-file', required=True,
-                   help=('CSV file with list of videos to process. Remove any '
+                   help=('TSV-file with list of videos to process. Remove any '
                          'header or comments. Use "\t" as separator if any.'))
     p.add_argument('-of', '--output-file', required=True,
-                   help='CSV-file with duration (s) and frame rate of videos')
+                   help=('TSV-file with video-name, duration (s), frame rate, '
+                         'number of frames of videos'))
     p.add_argument('-n', '--n-jobs', default=1, type=int,
-                   help='Number of CPUs')
+                   help='Number of process to spawn with joblib')
     args = p.parse_args()
     return args
 
@@ -38,10 +41,10 @@ def main(input_file, output_file, n_jobs):
     df = pd.read_csv(input_file, sep='\t', header=None)
     stats = Parallel(n_jobs=n_jobs)(delayed(video_stats)(i)
                                     for i in df.loc[:, 0])
-    df_stat = pd.DataFrame(np.array(stats),
-                           columns=['video-name', 'duration', 'frame-rate'])
-    new_df = pd.concat((df, df_stat), axis=1)
-    new_df.to_csv(output_file, sep='\t', index=False)
+    df_stat = pd.DataFrame(stats,
+                           columns=['video-name', 'duration', 'frame-rate',
+                                    'num-frames'])
+    df_stat.to_csv(output_file, sep='\t', index=False)
 
 
 if __name__ == '__main__':
