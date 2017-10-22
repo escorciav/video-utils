466d7a3c0f63f7c1d32e09fa1c558b5ccf821d1a
Victor Escorcia <escorciav@gmail.com>
2017-10-22 03:01:57 +0300

    [okvideo] Update ffmpeg module
    Simplify logic in ffmpeg module. Move all the sanity checks to programs
    or future abstractions.
    
    - Update script to dump frames of multiple videos

--- okvideo/ffmpeg.py
+++ okvideo/ffmpeg.py
@@ -1,50 +1,32 @@
 from subprocess import check_output
 import glob
+import logging
 import os
 import subprocess
 
 
-def dump_frames(filename, output_folder, output_format='%06d.png',
-                extra_filters=''):
-    """Dump frames of a video-file into a folder
+def dump_frames(filename, output_format='%06d.jpg', filters='-qscale:v 1'):
+    """Dump frames of a video-file
 
-    Parameters
-    ----------
-    filename : str
-        Fullpath of video-file
-    output_folder : str
-        Fullpath of folder to place frames
-    basename_format : str, optional
-        String format used to save video frames.
-    extra_filters : str, optional
-        Additional filters for ffmpeg such as "-vf scale=320x240".
+   Args:
+        filename (str): fullpath of video-file
+        basename_format (str, optional): string format used to save video
+            frames.
+        filters (str, optional): additional filters for ffmpeg e.g.
+            "-vf scale=320x240".
 
-    Outputs
-    -------
-    success : bool
-
-    Raises
-    ------
-    IOError
-        Unexistent video file
+    Returns:
+        success : bool
 
     """
-    if not os.path.isfile(filename):
-        raise IOError('Unexistent video {}'.format(filename))
-    if not os.path.isdir(output_folder):
-        os.makedirs(output_folder)
-    if len(extra_filters) > 1 and extra_filters[-1] != ' ':
-        extra_filters += ' '
-
-    output_format = os.path.join(output_folder, output_format)
-    cmd = ('ffmpeg -v error -i {} -qscale:v 2 {}-f image2 {}'.format(
-        filename, extra_filters, output_format)).split()
+    cmd = 'ffmpeg -v error -i {} {} {}'.format(
+        filename, filters, output_format).split()
 
     try:
         out = check_output(cmd, stderr=subprocess.STDOUT)
     except subprocess.CalledProcessError:
-        print('Imposible to dump video', filename)
-        print('Traceback:\n', out)
+        logging.debug('Imposible to dump video', filename)
+        logging.debug('Traceback:\n', out)
         return False
     return True
 
@@ -52,24 +34,13 @@ def dump_frames(filename, output_folder, output_format='%06d.png',
 def get_duration(filename):
     """Return duration on seconds of a video
 
-    Parameters
-    ----------
-    filename : str
-        Fullpath of video-file
+    Args:
+        filename (str): fullpath of video-file
 
-    Outputs
-    -------
-    frame_rate : float
-
-    Raises
-    ------
-    IOError
-        Unexistent video file
+    Returns:
+        frame_rate (float)
 
     """
-    if not os.path.isfile(filename):
-        raise IOError('Unexistent video {}'.format(filename))
-
     cmd = ('ffprobe -v 0 -of flat=s=_ -select_streams v:0 -show_entries '
            'stream=duration -of default=nokey=1:noprint_wrappers=1 ' +
            filename).split()
@@ -85,24 +56,13 @@ def get_duration(filename):
 def get_frame_rate(filename):
     """Return frame-rate of video
 
-    Parameters
-    ----------
-    filename : str
-        Fullpath of video-file
+    Args:
+        filename (str): Fullpath of video-file
 
-    Outputs
-    -------
-    frame_rate : float
-
-    Raises
-    ------
-    IOError
-        Unexistent video file
+    Returns:
+        frame_rate (float)
 
     """
-    if not os.path.isfile(filename):
-        raise IOError('Unexistent video {}'.format(filename))
-
     cmd = ('ffprobe -v 0 -of flat=s=_ -select_streams v:0 -show_entries '
            'stream=avg_frame_rate -of default=nokey=1:noprint_wrappers=1 ' +
            filename).split()
@@ -124,32 +84,25 @@ def get_frame_rate(filename):
 def get_num_frames(filename, ext='*.jpg'):
     """Count number of frames of a video
 
-    Parameters
-    ----------
-    filename : str
-        fullpath of video file
-    ext : str, optional
-        image extension
+    Args:
+        filename (str): fullpath of video file
+        ext (str, optional): image extension
 
-    Outputs
-    -------
-    counter : int
-        number of frames
+    Returns:
+        counter (int): number of frames
 
     Raises
-    ------
-    IOError
-        Unexistent video file
+        ValueError: unexpected filename
 
     """
     if os.path.isdir(filename):
         return len(glob.glob(os.path.join(filename, ext)))
-    elif not os.path.isfile(filename):
-        raise IOError('Unexistent video {}'.format(filename))
-
-    cmd = ('ffprobe -v error -count_frames -select_streams v:0 -show_entries '
-           'stream=nb_read_frames -of default=nokey=1:noprint_wrappers=1 ' +
-           filename)
-    nframes_expr = check_output(cmd, universal_newlines=True, shell=True)
-    nframes = int(nframes_expr.rstrip())
-    return nframes
+    elif os.path.isfile(filename):
+        cmd = ('ffprobe -v error -count_frames -select_streams v:0 '
+               '-show_entries stream=nb_read_frames -of '
+               'default=nokey=1:noprint_wrappers=1 ' + filename)
+        nframes_expr = check_output(cmd, universal_newlines=True, shell=True)
+        nframes = int(nframes_expr.rstrip())
+        return nframes
+    else:
+        raise ValueError('Unexpect filename: {}'.format(filename))
--- tools/batch_dump_frames.py
+++ tools/batch_dump_frames.py
@@ -5,6 +5,7 @@ Python program to dump frames of many videos
 
 """
 import argparse
+import logging
 import os
 
 from joblib import Parallel, delayed
@@ -13,48 +14,66 @@ import pandas as pd
 from okvideo.ffmpeg import dump_frames
 
 
-def dump_wrapper(filename, output_folder, baseformat, extra_filters,
-                 fullpath_flag, video_path):
+def main(input_file, output_folder, baseformat, filters, n_jobs,
+         fullpath_flag, video_path):
+    df = pd.read_csv(input_file, header=None)
+    if not os.path.isdir(output_folder):
+        os.makedirs(output_folder)
+    Parallel(n_jobs=n_jobs)(
+        delayed(dump_wrapper)(i, output_folder, baseformat, filters,
+                              fullpath_flag, video_path)
+        for i in df.loc[:, 0])
+    return None
+
+
+def dump_wrapper(filename, output_folder, baseformat, filters, fullpath_flag,
+                 video_path):
     filename_noext = os.path.splitext(filename)[0]
     if fullpath_flag:
         filename_noext = os.path.basename(filename_noext)
     if video_path:
         filename = os.path.join(video_path, filename)
     output_dir = os.path.join(output_folder, filename_noext)
-    return dump_frames(filename, output_dir, baseformat, extra_filters)
+    if not os.path.isfile(filename):
+        logging.debug('Unexistent file {}'.format(filename))
+        return False
+    if not os.path.isdir(output_dir):
+        os.makedirs(output_dir)
+    output = os.path.join(output_dir, baseformat)
+    return dump_frames(filename, output, filters)
 
 
-def input_parser():
-    description = ('Extract frames of a bunch of videos. The file-system '
-                   'organization is preserved if relative-path are used.')
-    p = argparse.ArgumentParser(description=description)
+if __name__ == '__main__':
+    p = argparse.ArgumentParser(
+        description=('Extract frames of a bunch of videos. The file-system '
+                     'organization is preserved if relative-path are used.'))
     p.add_argument('-if', '--input-file', required=True,
                    help='CSV file with list of videos to process.')
     p.add_argument('-of', '--output-folder', help='Folder to allocate frames')
-    p.add_argument('-bf', '--baseformat', default='%06d.png',
+    p.add_argument('-bf', '--baseformat', default='%06d.jpg',
                    help='Format used for naming frames e.g. %%06d.jpg')
-    p.add_argument('-ef', '--extra-filters', default='',
+    p.add_argument('--filters', default='',
                    help='Filters for ffmpeg')
+    # Workers
     p.add_argument('-n', '--n-jobs', default=1, type=int,
                    help='Max number of process')
+    # Video path
     p.add_argument('-ff', '--fullpath-flag', action='store_true',
                    help='Input-file uses fullpath instead of rel-path')
     p.add_argument('-vpath', '--video-path', default=None,
                    help='Path where the videos are located.')
+    # Logging
+    p.add_argument('-log', '--loglevel', default='INFO',
+                   help='verbosity level')
     args = p.parse_args()
-    return args
 
+    numeric_level = getattr(logging, args.loglevel.upper(), None)
+    if not isinstance(numeric_level, int):
+        raise ValueError('Invalid log level: %s' % args.loglevel)
+    logging.basicConfig(
+        format=('%(asctime)s %(levelname)-8s [%(filename)s:%(lineno)d]: '
+                '%(message)s'),
+        level=numeric_level)
+    delattr(args, 'loglevel')
 
-def main(input_file, output_folder, baseformat, extra_filters, n_jobs,
-         fullpath_flag, video_path):
-    df = pd.read_csv(input_file, sep='\t', header=None)
-    Parallel(n_jobs=n_jobs)(
-        delayed(dump_wrapper)(i, output_folder, baseformat, extra_filters,
-                              fullpath_flag, video_path)
-        for i in df.loc[:, 0])
-    return None
-
-
-if __name__ == '__main__':
-    args = input_parser()
     main(**vars(args))
